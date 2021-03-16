7f143ca38a3d3c6e20812c2579451b6827112c8c
chriswang0401 <80297628+chriswang0401@users.noreply.github.com>
2021-03-15 17:16:32 -0700

    Add files via upload

---
+++ testapp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: chriswang0401
+"""
+
+import streamlit as sl
+
+sl.title("Welcome to MA346: Data Science")
+sl.header("-------------------------------------------------")
+sl.success("Sadly, very little school maths focuses on how to win free drinks "
+           + "in a pub. -Matt Pakre, Things to Make and Do in the Fourth Dimension")
+sl.info("This is a brief on how to publish a web-app built in python using " +
+        "Streamlit and Heroku.")
